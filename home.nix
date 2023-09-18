4f24715b24705feb7904131f271cdf9006795b9f
Daniel Bonofiglio <dev@dan.uy>
2023-09-18 15:01:33 -0300

    update kitty style

--- home.nix
+++ home.nix
@@ -105,14 +105,18 @@
       extraConfig = ''
         map cmd+t no_op
         map cmd+t launch --cwd=current --type=tab
-        tab_bar_style powerline
-        tab_powerline_style round
         window_padding_width 0 0 0 1
+        tab_bar_min_tabs            1
+        tab_bar_edge                bottom
+        tab_bar_style               powerline
+        tab_powerline_style         slanted
+        tab_title_template          {title}{' :{}:'.format(num_windows) if num_windows > 1 else \'\'}
       '';
       font = {
         name = "JetBrainsMonoNL Nerd Font Mono";
         size = 20;
       };
+      theme = "Catppuccin-Mocha";
   };
 
   # SSH
