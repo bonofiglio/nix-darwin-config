ee765225890abf8b71e6b6f218182209be67e6f8
Daniel Bonofiglio <dev@dan.uy>
2023-10-07 15:42:47 -0300

    move input sources config to `defaults` folder and `toXml` to `lib` folder

---
+++ defaults/input-sources.nix
@@ -0,0 +1,17 @@
+let
+toXml = import ../lib/to-xml.nix;
+in
+''
+defaults write com.apple.HIToolbox AppleEnabledInputSources "${toXml [
+    {
+            InputSourceKind = "Keyboard Layout";
+            "KeyboardLayout ID" = 252;
+            "KeyboardLayout Name" = "ABC";
+    }
+    {
+        InputSourceKind = "Keyboard Layout";
+        "KeyboardLayout ID" = 87;
+        "KeyboardLayout Name" = "Spanish - ISO";
+    }
+]}"
+''
