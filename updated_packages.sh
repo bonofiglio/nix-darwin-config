8a9f66b7e02d5c9b84c5893aec34df5b0745fad3
Daniel Bonofiglio <dev@dan.uy>
2024-02-14 14:49:15 -0300

    feat: add changelog script

---
+++ updated_packages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+arguments="$@"
+
+# Print usage when arguments are not correct or '-h' is provided
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 [reference commit hash (default: HEAD~1)] [new commit hash (default: HEAD)]"
+    exit 1
+fi
+
+referenceCommitHash="$1"
+
+if [ -z "$referenceCommitHash" ]; then
+    referenceCommitHash="$(git rev-parse HEAD~1)"
+fi
+
+newCommitHash="$2"
+
+if [ -z "$newCommitHash" ]; then
+    newCommitHash="HEAD"
+fi
+
+# Error when an unknown argument is provided
+if [ "$3" ]; then
+    echo "Unknown argument: $3"
+    exit 1
+fi
+
+
+# Get the list of packages that have been updated in the new commit
+nix store diff-closures ".?ref=$referenceCommitHash#darwinConfigurations.\"Daniels-MacBook-Pro\".config.system.build.toplevel" ".?ref=$newCommitHash#darwinConfigurations.\"Daniels-MacBook-Pro\".config.system.build.toplevel"
