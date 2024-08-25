d968c0a28080c60871dcdeb79de56108d90527c7
Daniel Bonofiglio <dev@dan.uy>
2024-08-25 14:22:42 -0300

    fix: use correct shell

--- updated_packages.sh
+++ updated_packages.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-arguments="$@"
+#!/bin/sh
 
 # Print usage when arguments are not correct or '-h' is provided
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
@@ -25,7 +23,6 @@ if [ "$3" ]; then
     echo "Unknown argument: $3"
     exit 1
 fi
-
 
 # Get the list of packages that have been updated in the new commit
 nix store diff-closures ".?ref=$referenceCommitHash#darwinConfigurations.\"Daniels-MacBook-Pro\".config.system.build.toplevel" ".?ref=$newCommitHash#darwinConfigurations.\"Daniels-MacBook-Pro\".config.system.build.toplevel"
